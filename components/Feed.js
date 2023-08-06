4842b6d002cdd0a10edd1003cb41667e5d010871
AllenChang7 <allenychang2003@gmail.com>
2023-08-06 18:43:34 +0800

    UI changes

--- components/Feed.js
+++ components/Feed.js
@@ -160,7 +160,7 @@ const Feed = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             class="w-6 h-6"
           >
