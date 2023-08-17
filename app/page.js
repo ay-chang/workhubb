9c3203b0364eaac6e4312e360375d251fcd4e0e4
AllenChang7 <allenychang2003@gmail.com>
2023-08-18 00:05:08 +0800

    New Feed UI

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Feed from "@components/Feed";
+import TestFeed from "@components/testFeed";
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
+    <section className="w-full flex-col flex-center">
       <h1 className="head__text text-center">
         Welcome to the <br className="max-md:hidden" />
         <span className="text-center blue__gradient">Work Hubb</span>
@@ -12,7 +13,8 @@ const Home = () => {
         This is a platform designed to help freelancers find work and to help customers
         creating work to pick the right price for them!
       </p>
-      <Feed />
+      <TestFeed />
+      {/* <Feed /> */}
     </section>
   );
 };
